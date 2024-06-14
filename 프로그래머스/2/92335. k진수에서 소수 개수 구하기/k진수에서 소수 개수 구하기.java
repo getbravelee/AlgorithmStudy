51352ba660ded51a61697d7e67f849a4441023cf
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-06-14 18:31:48 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.10 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,5 +1,3 @@
-import java.math.BigInteger;
-
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
@@ -9,28 +7,32 @@ class Solution {
         for (String str : arr) {
             // 빈 문자열을 무시
             if (!str.isEmpty()) {
-                BigInteger num = new BigInteger(str);
-                if (isPrime(num)) {
-                    answer++;
+                try {
+                    long num = Long.parseLong(str);
+                    if (isPrime(num)) {
+                        answer++;
+                    }
+                } catch (NumberFormatException e) {
+                    // 숫자가 너무 커서 long으로 변환할 수 없을 때
+                    continue;
                 }
             }
         }
         return answer;
     }
 
-    public boolean isPrime(BigInteger num) {
+    public boolean isPrime(long num) {
         // 0과 1은 소수가 아니다
-        if (num.compareTo(BigInteger.TWO) < 0) {
+        if (num < 2) {
             return false;
         }
         // 2는 소수이다
-        if (num.equals(BigInteger.TWO)) {
+        if (num == 2) {
             return true;
         }
         // 2 이상의 숫자에 대해 소수 판별
-        BigInteger sqrt = num.sqrt();
-        for (BigInteger i = BigInteger.TWO; i.compareTo(sqrt) <= 0; i = i.add(BigInteger.ONE)) {
-            if (num.mod(i).equals(BigInteger.ZERO)) {
+        for (long i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
