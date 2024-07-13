841d2069c01af6d0018bb961c83835e4e3429520
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-07-13 12:49:48 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 872 ms, Memory: 288236 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < t; tc++) {
+            String[] s = br.readLine().split(" ");
+            int a = Integer.parseInt(s[0]);
+            int b = Integer.parseInt(s[1]);
+            int result = a+b;
+            bw.write(result + "\n");
+        }
+        bw.flush();
+    }
+}
