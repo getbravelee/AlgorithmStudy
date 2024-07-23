f6dc6999bab2104bdef2f980eaf2657e29e2233b
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-07-23 11:42:05 +0900

    [Silver IV] Title: 국영수, Time: 1484 ms, Memory: 64356 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Student> list = new ArrayList<>();
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			int kor = Integer.parseInt(st.nextToken());
+			int eng = Integer.parseInt(st.nextToken());
+			int math = Integer.parseInt(st.nextToken());
+
+			list.add(new Student(name, kor, eng, math));
+		}
+		Collections.sort(list, new Comparator<Student>() {
+			@Override
+			public int compare(Student o1, Student o2) {
+				if(o1.kor != o2.kor) {
+					return o2.kor - o1.kor;
+				}
+				else if(o1.eng != o2.eng) {
+					return o1.eng - o2.eng;
+				}
+				else if(o1.math != o2.math) {
+					return o2.math - o1.math;
+				}
+				else {
+					return o1.name.compareTo(o2.name);
+				}
+			}
+		});
+		for(Student s : list) {
+			System.out.println(s.name);
+		}
+	}
+	
+	
+	static class Student {
+		String name;
+		int kor, eng, math;
+		
+		public Student(String name, int kor, int eng, int math) {
+			this.name = name;
+			this.kor = kor;
+			this.eng = eng;
+			this.math = math;
+		}
+	}
+}
