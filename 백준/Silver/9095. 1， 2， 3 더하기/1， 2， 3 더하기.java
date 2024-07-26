90d33b8a48f5b84855f14bb6770e1ff1dd0dce43
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-07-26 15:39:09 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		StringTokenizer st;
+		int tc = Integer.parseInt(br.readLine());
+		while(tc-->0) {
+			int n = Integer.parseInt(br.readLine());
+			System.out.println(dp(n));
+		}
+	}
+	
+	public static int dp(int n) {
+		if(n == 1) return 1;
+		else if(n==2) return 2;
+		else if(n==3) return 4;
+
+		return dp(n-1)+dp(n-2)+dp(n-3);
+	}
+}
