0a5d48eb6b7cda135ead6502208dce87f022239c
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-08-27 12:01:52 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 472 ms, Memory: 48468 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -5,87 +5,79 @@ public class Main {
 
     static int[] parent;
 
-    static class Node implements Comparable<Node> {
+    static class Edge implements Comparable<Edge>{
         int to, from, value;
 
-        public Node(int to, int from, int value) {
+        public Edge(int to, int from, int value) {
             this.to = to;
             this.from = from;
             this.value = value;
         }
 
-        public int compareTo(Node o) {
-            return this.value - o.value;
+        public int compareTo(Edge e) {
+            return this.value - e.value;
         }
     }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
 
-        parent = new int[V+1];
+        Queue<Edge> pq = new PriorityQueue<>();
+
+        parent = new int[V + 1];
         for (int i = 1; i <= V; i++) {
             parent[i] = i;
         }
 
-        Queue<Node> pq = new PriorityQueue<>();
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
+            int vA = Integer.parseInt(st.nextToken());
+            int vB = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            int t = Integer.parseInt(st.nextToken());
-            int f = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            pq.offer(new Node(t, f, v));
+            pq.offer(new Edge(vA, vB, w));
         }
 
         int total = 0;
 
-        while (!pq.isEmpty()) {
-            Node tmp = pq.poll();
+        while(!pq.isEmpty()) {
+            Edge tmp = pq.poll();
+
             int to = find(tmp.to);
             int from = find(tmp.from);
 
-            if (!isSameParent(to, from)) {
+            if(!isSameParent(to, from)) {
                 total += tmp.value;
-                union(tmp.to, tmp.from);
+                union(to, from);
             }
         }
 
         System.out.println(total);
-
     }
 
     public static int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        else {
-            return parent[x] = find(parent[x]);
-        }
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
 
-    public static void union(int x, int y) {
+     static void union(int x, int y) {
         x = find(x);
         y = find(y);
 
-        if (x != y) {
-            parent[y] = x;
+        if(x<y) {
+            parent[y]=x;
+        }
+        else {
+            parent[x] =y;
         }
     }
 
     public static boolean isSameParent(int x, int y) {
         x = find(x);
         y = find(y);
-
-        if (x == y) {
-            return true;
-        }
-
-        else {
-            return false;
-        }
+        if(x == y) return true;
+        else return false;
     }
 }
