3fa845d1251b9e558595704bb02665d1e36e5c23
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-10-10 10:44:38 +0900

    [Gold III] Title: 학교 탐방하기, Time: 920 ms, Memory: 179020 KB -BaekjoonHub

--- 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.java
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.java
@@ -3,11 +3,11 @@ import java.util.*;
 
 public class Main {
 
-    static int N, M;
+    static int N,M;
     static int[] parents;
 
     static class Node {
-        int from, to, value;
+        int from , to, value;
 
         public Node(int from, int to, int value) {
             this.from = from;
@@ -22,8 +22,9 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Node> pq1 = new PriorityQueue<>((o1, o2) -> o1.value - o2.value); // 최적의 코스
-        PriorityQueue<Node> pq2 = new PriorityQueue<>((o1, o2) -> o2.value - o1.value); // 최악의 코스
+        PriorityQueue<Node> pq1 = new PriorityQueue<>((o1, o2) -> o1.value - o2.value);
+        PriorityQueue<Node> pq2 = new PriorityQueue<>((o1, o2) -> o2.value - o1.value);
+
 
         for (int i = 0; i < M + 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -34,9 +35,8 @@ public class Main {
             pq1.add(new Node(v1, v2, c));
             pq2.add(new Node(v1, v2, c));
         }
-
         make();
-        int best = 0;  // 최적의 코스에서 오르막길 개수
+        int uphill = 0;  // 오르막길 카운트
 
         while (!pq1.isEmpty()) {
             Node tmp = pq1.poll();
@@ -44,15 +44,14 @@ public class Main {
             int to = tmp.to;
 
             if (!isSameParent(from, to)) {
-                if (tmp.value == 0) {  // 내리막길
-                    best++;
+                if (tmp.value == 0) {
+                    uphill++;
                 }
                 union(from, to);
             }
         }
-
         make();
-        int worst = 0;  // 최악의 코스에서 오르막길 개수
+        int downhill = 0;  // 내리막길 카운트
 
         while (!pq2.isEmpty()) {
             Node tmp = pq2.poll();
@@ -60,37 +59,43 @@ public class Main {
             int to = tmp.to;
 
             if (!isSameParent(from, to)) {
-                if (tmp.value == 0) {  // 내리막길
-                    worst++;
+                if (tmp.value == 0) {
+                    downhill++;
                 }
                 union(from, to);
             }
         }
-
-        // 피로도의 차이 계산
-        int result = Math.abs(worst * worst - best * best);
+        int result = (int)Math.abs(Math.pow(uphill,2) - Math.pow(downhill,2));
         System.out.println(result);
-    }
 
+    }
     static void make() {
         parents = new int[N + 1];
+
         for (int i = 0; i <= N; i++) {
             parents[i] = i;
         }
     }
 
     static int find(int x) {
-        if (parents[x] == x) return x;
+        if(parents[x] ==  x) return x;
         return parents[x] = find(parents[x]);
     }
 
     static void union(int x, int y) {
         x = find(x);
         y = find(y);
-        if (x != y) parents[y] = x;
+
+//        if(x <= y) parents[x] = y;
+//        else parents[y] = x;
+        if(x != y) parents[y] = x;
     }
 
     static boolean isSameParent(int x, int y) {
-        return find(x) == find(y);
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return true;
+        return false;
     }
 }
