d4cb72a87742be5d26899f1885ad62bd5ded2b58
getbravelee <136793519+getbravelee@users.noreply.github.com>
2024-11-05 10:04:20 +0900

    [Gold III] Title: 불!, Time: 448 ms, Memory: 66556 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -3,20 +3,21 @@ import java.util.*;
 
 public class Main {
 
-    static int R, C;
+    static int R,C;
     static char[][] map;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static boolean[][] visitedJ, visitedF;
-    static Queue<Point> jq = new LinkedList<>();
-    static Queue<Point> fq = new LinkedList<>();
+    static boolean[][] visited;
+    static int cnt;
 
     static class Point {
         int x, y;
+        boolean isFire;
 
-        public Point(int x, int y) {
+        public Point(int x, int y, boolean isFire) {
             this.x = x;
             this.y = y;
+            this.isFire = isFire;
         }
     }
 
@@ -26,75 +27,72 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         map = new char[R][C];
-        visitedJ = new boolean[R][C];
-        visitedF = new boolean[R][C];
-        
-        int x = 0, y = 0;
-
-        // 초기 지도 상태 입력 및 불과 지훈 위치 초기화
-        for (int i = 0; i < R; i++) {
+        visited = new boolean[R][C];
+        cnt = 1;
+        // 지훈이 시작위치
+        int x = 0;
+        int y = 0;
+        Queue<Point> q = new LinkedList<>();
+        for(int i = 0; i < R; i++) {
             String s = br.readLine();
             for (int j = 0; j < C; j++) {
                 map[i][j] = s.charAt(j);
-                if (map[i][j] == 'J') {
+                char c = map[i][j];
+                if (c == 'J') {
                     x = i;
                     y = j;
-                    visitedJ[i][j] = true;
-                    jq.add(new Point(i, j));
-                } else if (map[i][j] == 'F') {
-                    visitedF[i][j] = true;
-                    fq.add(new Point(i, j));
+                }
+                else if (c == '#') {
+                    visited[i][j] = true;
+                }
+                else if (c == 'F') {
+                    visited[i][j] = true;
+                    q.add(new Point(i, j, true));
                 }
             }
         }
 
-        int result = bfs();
-        System.out.println(result == -1 ? "IMPOSSIBLE" : result);
+        if(x == 0 || x == R-1 || y == 0 || y == C-1) System.out.println(cnt);
+        else {
+            boolean game = bfs(x, y, q);
+            if(!game) System.out.println("IMPOSSIBLE ");
+            else System.out.println(cnt);
+        }
+
     }
 
-    static int bfs() {
-        int time = 0;
+    static boolean bfs(int x, int y, Queue<Point> q) {
+        q.add(new Point(x, y, false));
 
-        while (!jq.isEmpty()) {
-            time++;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            cnt++;
+            for (int k = 0; k < size; k++) {
+                Point tmp = q.poll();
 
-            // 먼저 불이 퍼지는 과정
-            int fireSize = fq.size();
-            for (int f = 0; f < fireSize; f++) {
-                Point fire = fq.poll();
-                for (int d = 0; d < 4; d++) {
-                    int nx = fire.x + dx[d];
-                    int ny = fire.y + dy[d];
+                for (int i = 0; i < 4; i++) {
+                    int nx = tmp.x + dx[i];
+                    int ny = tmp.y + dy[i];
 
-                    if (nx >= 0 && nx < R && ny >= 0 && ny < C && !visitedF[nx][ny] && map[nx][ny] != '#') {
-                        visitedF[nx][ny] = true;
-                        fq.add(new Point(nx, ny));
-                    }
-                }
-            }
+                    if (nx >= 0 && nx < R && ny >= 0 && ny < C && !visited[nx][ny]) {
+                        if (!tmp.isFire) {
+                            if (nx == 0 || nx == R - 1 || ny == 0 || ny == C - 1) {
+                                return true;
+                            }
+                            if (map[nx][ny] == '.') {
+                                visited[nx][ny] = true;
+                                q.add(new Point(nx, ny, false));
+                            }
+                        }
+                        else {
+                            visited[nx][ny] = true;
+                            q.add(new Point(nx, ny, true));
+                        }
 
-            // 지훈이가 이동하는 과정
-            int jihoonSize = jq.size();
-            for (int j = 0; j < jihoonSize; j++) {
-                Point jihoon = jq.poll();
-                for (int d = 0; d < 4; d++) {
-                    int nx = jihoon.x + dx[d];
-                    int ny = jihoon.y + dy[d];
-
-                    // 지훈이가 탈출할 수 있는 경우
-                    if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
-                        return time;
-                    }
-
-                    // 다음 위치가 빈 공간이고 방문하지 않았으며, 불이 아직 도달하지 않은 경우
-                    if (!visitedJ[nx][ny] && !visitedF[nx][ny] && map[nx][ny] == '.') {
-                        visitedJ[nx][ny] = true;
-                        jq.add(new Point(nx, ny));
                     }
                 }
             }
         }
-
-        return -1;
+        return false;
     }
 }
