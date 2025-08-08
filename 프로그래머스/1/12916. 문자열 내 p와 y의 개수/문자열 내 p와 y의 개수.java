1bb312b6b8830efb1c7d388d98f420109bd009ab
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-08 18:15:49 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.04 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,24 +1,20 @@
-/**
-1. 배열을 소문자로 초기화하여 원소 하나씩 비교하는 방법
-2. 문자열을 그대로 소문자로 변환하여 charAt()으로 비교하는 방법
-**/ 
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        // p, y 카운트
-        int p = 0;
-        int y = 0;
-        // 소문자로 변환하여 배열 초기화
-        String[] lowerS = s.toLowerCase().split("");
-        // 배열을 돌며 p 또는 y 인지 카운트
-        for(int i = 0; i < lowerS.length; i++) {
-            if(lowerS[i].equals("p")) {p += 1;}
-            else if(lowerS[i].equals("y")) {y += 1;}
+        int countP = 0;
+        int countY = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == 'p' || c == 'P') {
+                countP++;
+            }
+            else if(c == 'y' || c == 'Y') {
+                countY++;
+            }
         }
-        // p, y 카운트가 다르면 false
-        if (y != p) {answer = false;}
-        
+        if(countP != countY) {
+            answer = false;
+        }
         return answer;
     }
 }
