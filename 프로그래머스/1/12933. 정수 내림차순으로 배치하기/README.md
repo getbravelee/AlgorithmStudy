e98eb4e37fbceea3dc22be873d10c1b6475fccd1
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-08 19:16:34 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 2.08 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
 
 ### 성능 요약
 
-메모리: 77 MB, 시간: 6.74 ms
+메모리: 71.4 MB, 시간: 2.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 20일 19:52:36
+2025년 08월 08일 19:16:32
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,23 +1,22 @@
 import java.util.*;
-
-public class Solution {
+class Solution {
     public long solution(long n) {
         long answer = 0;
-        // 문자열로 바꾸기
-        String s = "" + n;
-        // 내림차순 정렬하여 문자열로 초기화할 변수
-        String revSortedList = "";
-        // 원소 크기를 비교하여 오름차순 정렬하기 위해 배열 초기화
-        String[] nList = s.split("");
-        // 정렬
-        Arrays.sort(nList);
-        // 오름차순으로 정렬된 원소 역순으로 문자열로 초기화
-        for(int i = s.length() - 1; i >= 0; i--) {
-            revSortedList += nList[i];
+        String s = String.valueOf(n);
+        int[] arr = new int[s.length()];
+        for(int i = 0; i < s.length();i ++) {
+            arr[i] = s.charAt(i) - 48;
+        } 
+        Arrays.sort(arr);
+        int[] temp = new int[s.length()];
+        for(int i = s.length()-1; i >= 0; i--) {
+            temp[s.length()- 1 - i] = arr[i];
         }
-        System.out.println(revSortedList);
-        // 문자열을 정수로 변환
-        answer = Long.parseLong(revSortedList);
+        String str = "";
+        for(int i = 0; i < s.length();i ++) {
+            str += String.valueOf(temp[i]);
+        } 
+        answer = Long.parseLong(str);
         return answer;
     }
 }
