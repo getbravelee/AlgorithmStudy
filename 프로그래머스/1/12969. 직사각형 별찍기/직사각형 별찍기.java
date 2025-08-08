43bee67022c76f7a5a9eecf108be1ade84545ebc
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-09 02:29:29 +0900

    [level 1] Title: 직사각형 별찍기, Time: 149.63 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        
+        for(int i = 0; i < b; i++) {
+            String s = "";
+            for(int j = 0; j < a; j++) {
+                s += "*";
+            }
+            System.out.println(s);
+        }
+        
+    }
+}
