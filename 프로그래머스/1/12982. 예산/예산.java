69916fb41ed031f2657566130c04c7ec8e38fb24
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-09 21:07:00 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,17 +1,18 @@
-import java.lang.Math;
 import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
-        int cnt = 0;
-        int sum = 0;
-        // 오름차순 정렬 -> 가장 낮은 값 부터 더하면 최대 카운트이다.
+        int answer = 0;
         Arrays.sort(d);
         for(int i = 0; i < d.length; i++) {
-            if(sum + d[i] <= budget) {
-                sum += d[i];
-                cnt += 1;
+            if(budget - d[i] >= 0) {
+                budget -= d[i];
+                answer++;
             }
+            else {
+                break;
+            }
+            
         }
-        return cnt;
+        return answer;
     }
 }
