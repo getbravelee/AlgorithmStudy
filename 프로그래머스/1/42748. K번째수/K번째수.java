c1c3541263fca0e3858692ee9cbbdf156714bc06
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-11 00:47:19 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,20 +1,24 @@
-/*
-[명령][i,j,k]
-?번째 : 인덱스 + 1
-자르기 -> 오름차순 정렬 -> k번째 수
-*/
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        ArrayList<Integer> list = new ArrayList<>();
+        
         for(int i = 0; i < commands.length; i++) {
-            for(int j = commands[i][0]-1; j <= commands[i][1]-1; j++) {
+            List<Integer> list = new ArrayList<>();
+            
+            for(int j = commands[i][0] - 1; j < commands[i][1]; j++) {
                 list.add(array[j]);
             }
+            
             Collections.sort(list);
-            answer[i] = list.get(commands[i][2]-1);
-            list.clear();
+            int idx = 1;
+            
+            for(int n : list) {
+                if(idx == commands[i][2]) {
+                    answer[i] = n;
+                }
+                idx++;
+            }
         }
         return answer;
     }
