5a169c88d28b59ae86c922b20fa2e729207ee3a7
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-11 00:15:19 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.79 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,18 @@
-// food의 길이는 4가 아니다. 2~9 이다. 주의.
 class Solution {
     public String solution(int[] food) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i < food.length; i++) {
-            int cnt = food[i] / 2;
-            sb.append(String.valueOf(i).repeat(cnt));
+            for(int j = 0; j < food[i]/2; j++) {
+                sb.append(i);
+            }
         }
-        answer = sb + "0";
-        answer += sb.reverse();
+        sb.append("0");
+        String s = sb.toString();
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(s.substring(0, s.length()-1));
+        sb.append(sb2.reverse());
+        answer = sb.toString();
         return answer;
     }
 }
