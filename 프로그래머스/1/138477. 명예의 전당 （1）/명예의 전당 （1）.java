8b45d022764728ec0eb9433d040209da4749bbc0
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-12 05:32:44 +0900

    [level 1] Title: 명예의 전당 (1), Time: 50.59 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,21 +1,35 @@
-/*
-
-*/
 import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] ranks = new int[2];
+        List<Integer> list = new ArrayList<>();
+        // 두 가지 풀이 방법
+        // 1. ArrayList에 score 계속 넣고 정렬하고 score길이만큼 계속 반복해서 k순위 값만 result에 넣기
+        // 2. k크기 만큼의 ranks 배열 만들어서 k순위 값과 계속 비교하면서 갱신 -> k일이 되기 전까진 최소값이 발표점수이다.
+        int min = 2001;
         for(int i = 0; i < score.length; i++) {
             list.add(score[i]);
-            Collections.sort(list);
-            if(list.size() < k+1) {
-                answer[i] = list.get(0);
+            if(i < k) {
+                if(min < score[i]) {
+                    ranks[0] = score[i];
+                    ranks[1] = min;
+                }
+                else {
+                    min = score[i];
+                    ranks[0] = min;
+                    ranks[1] = score[i];
+                }
+                answer[i] = ranks[1];
             }
             else {
-                answer[i] = list.get(list.size()-k);
+                Collections.sort(list);
+                Collections.reverse(list);
+                answer[i] = list.get(k-1);
             }
         }
+        
+        
         return answer;
     }
 }
