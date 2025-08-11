1085cc332bda330fb3fbb78e6d41d41802f48660
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-12 07:23:40 +0900

    [level 1] Title: [1차] 비밀지도, Time: 16.46 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,25 +1,12 @@
-// 크기 n*n 2차원 배열 초기화
-// arr1에서 배열의 원소 해독하여 #을 찾는다
-// 찾은 #을 처음에 초기화한 배열에 알맞은 자리에 저장한다
-// arr2도 같은 방법으로 한다. 단, 이미 #이 있으면 넘어간다.
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        
         for(int i = 0; i < n; i++) {
-           String b1 = String.format("%" + n + "s", Integer.toBinaryString(arr1[i])).replace(" ", "0");
-            String b2 = String.format("%" + n + "s", Integer.toBinaryString(arr2[i])).replace(" ", "0");       
-            String s = "";
+            answer[i] = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]));
             for(int j = 0; j < n; j++) {
-                if(b1.charAt(j) == '1' || b2.charAt(j) == '1') {
-                    s += "#";
-                } 
-                else {
-                    s += " ";
-                }
+                answer[i] = answer[i].replaceAll("1", "#");
+                answer[i] = answer[i].replaceAll("0", " ");
             }
-            answer[i] = s;
         }
         return answer;
     }
