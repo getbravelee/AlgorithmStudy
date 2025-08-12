6a6f961d9206cdbc36a97a545ff8bf8b176fa184
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-12 11:29:25 +0900

    [level 1] Title: 폰켓몬, Time: 9.47 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            set.add(num);
+        }
+        int maxCount = set.size();
+        if(maxCount >= nums.length/2) {
+            answer = nums.length/2;
+        }
+        else {
+            answer = maxCount;
+        }
+        return answer;
+    }
+}
