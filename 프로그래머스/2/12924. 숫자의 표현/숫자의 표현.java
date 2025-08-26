cb87caff6f0b91190f5036ca399ab94c3c8c72db
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-26 21:57:10 +0900

    [level 2] Title: 숫자의 표현, Time: 1.56 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,17 +1,6 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 1; i <= n; i++) {
-            int sum = 0;
-            int num = i;
-            while(sum <= n) {
-                if(sum == n) {
-                    answer++;
-                    break;
-                }
-                sum += num++;
-            }
-        }
         return answer;
     }
 }
