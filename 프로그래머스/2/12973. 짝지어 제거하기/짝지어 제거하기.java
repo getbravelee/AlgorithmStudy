9b65c7c2aa48ecd2b7ad22c93da3f5611f6008d7
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-08-27 10:04:36 +0900

    [level 2] Title: 짝지어 제거하기, Time: 65.68 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 1;
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(stack.isEmpty()) {
+                stack.push(c);
+            }
+            else {
+                if(stack.peek() == c) {
+                    stack.pop();
+                }
+                else {
+                    stack.push(c);
+                }
+            }
+        }
+        if(!stack.isEmpty()) {
+            answer = 0;
+        }
+        return answer;
+    }
+}
