e8aeda268e12cf1f318f81f0c5302df38ff7cac0
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-09-02 21:28:44 +0900

    [level 2] Title: 귤 고르기, Time: 70.92 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,30 +1,20 @@
-/*
-tangerine을 map으로 최대 값을 찾는다. 
-조건1) 최대 값이 k보다 크거나 같은가 -> 카운트 ++ 해서 리턴
-조건2) 그렇지 않다면 k - 최대값으로 k의 값을 초기화 하고, 그 뺀 값만큼 해당하는 크기의 과일의 값을 뺀다.
-그리고 다시 조건1 부터 반복해서 카운트한다.
-*/
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        // 제일 많은 사이즈의 개수부터 k에서 빼기
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < tangerine.length; i++) {
-            int fruit = tangerine[i];
-            map.put(fruit, map.getOrDefault(fruit, 0) + 1);
+            map.put(tangerine[i], map.getOrDefault(tangerine[i], 0)+1);
         }
-        // 최대 값 찾기
-        ArrayList<Integer> list = new ArrayList<>(map.values());
-        Collections.sort(list, Collections.reverseOrder());
-        for(int v : list) {
-            if(k <= v) {
-                answer++;
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
+        for(int n : list) {
+            if(k <= 0) {
                 break;
             }
-            else {
-                k -= v;
-                answer++;
-            }
+            k -= map.get(n);
+            answer++;
         }
         return answer;
     }
