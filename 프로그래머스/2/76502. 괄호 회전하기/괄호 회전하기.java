31234404ed953c0b539450ebb2b5c936f36c86b3
SuHwan Lee <136793519+getbravelee@users.noreply.github.com>
2025-09-20 16:00:21 +0900

    [level 2] Title: 괄호 회전하기, Time: 257.93 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,32 +3,47 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        for(int i = 0; i < s.length(); i++) {
-            Stack<Character> st = new Stack<>();
-            String str = s.substring(i, s.length()) + s.substring(0, i);
-            
-            for(int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                if(st.isEmpty()) {
-                    st.push(c);
-                }
-                else if(c == ')' && st.peek() == '(') {
-                    st.pop();
-                }
-                else if(c == '}' && st.peek() == '{')  {
-                    st.pop();
-                }
-                else if(c == ']' && st.peek() == '[')  {
-                    st.pop();
-                }
-                else {
-                    st.push(c);
-                }
+        int len = s.length();
+         // 회전(0부터 문자열 길이만큼)
+        for(int i = 0; i < len; i++) {
+            String str = "";
+            for(int j = i; j < len+i; j++) {
+                char c = s.charAt(j%len);
+                str += Character.toString(c);
             }
-            if(st.isEmpty()) {
+
+            // 올바른 문자열인지 판단
+            if(isCorrect(str)) {
                 answer++;
             }
         }
+        
         return answer;
+    }
+    
+    private boolean isCorrect(String str) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(c == '[' || c == '{' || c == '(') {
+                st.push(c);
+            }
+            else {
+                if(c == ']' && (st.isEmpty() || st.pop() != '[')) {
+                    return false;
+                }
+                else if(c == '}' && (st.isEmpty() || st.pop() != '{')) {
+                    return false;
+                }
+                else if(c == ')' && (st.isEmpty() || st.pop() != '(')) {
+                    return false;
+                }
+            }
+        }
+        if(st.size() > 0) {
+            return false;
+        }
+        return true;
     }
 }
